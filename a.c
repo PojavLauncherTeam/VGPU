ca2ac845294449269aaa76a6c6b58b9847d60cc8
luo9674 <85796761+luo9674@users.noreply.github.com>
2021-06-13 23:30:21 -0500

    test_dynamic link library

---
+++ a.c
@@ -0,0 +1,55 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+ 
+ int a(int,int);
+ 
+ printf("        %d     /n", a(2,1));
+ 
+ return 0;
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
